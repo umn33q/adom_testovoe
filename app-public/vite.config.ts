15c246eb1a1bd21935905337da2f7eac0019031f
Your Name <you@example.com>
2025-10-30 10:44:11 +0300

    hmr fix

--- app-public/vite.config.ts
+++ app-public/vite.config.ts
@@ -10,6 +10,20 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
+      protocol: 'ws',
+      port: 5173,
+      clientPort: 5173,
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
