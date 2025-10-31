cca14a87014966f05edce3e5fb6b9d8dcc82953e
Your Name <you@example.com>
2025-10-31 11:41:05 +0300

    Установил reverb

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
             __DIR__.'/../routes/api/admin.php',
         ],
         commands: __DIR__.'/../routes/console.php',
+        channels: __DIR__.'/../routes/channels.php',
         // health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
